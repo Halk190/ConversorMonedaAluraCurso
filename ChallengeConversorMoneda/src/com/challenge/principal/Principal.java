754baae6283084c39b066f5e37a0386dfc04397f
Angel David Rojas Riveros <115513456+Halk190@users.noreply.github.com>
2024-10-24 09:43:00 -0500

    Main

---
+++ ChallengeConversorMoneda/src/com/challenge/principal/Principal.java
@@ -0,0 +1,21 @@
+package com.challenge.principal;
+
+public class Principal {
+    public static void main(String[] args) {
+        /*
+        Bienvenida al conversor de monedas
+        Menu de opciones de conversion
+        Dolar-pesoArg
+        pesoArg-Dolar
+        Dolar-RealBrasileño
+        RealBrasileño-Dolar
+        Dolar-pesoColombiano
+        PesoColombiano-Dolar
+        Salir
+        Mensaje de Elegir una opcion valida
+        Luego de seleccionar la opcion un mensaje de ingresar el valor a convertir
+        mensaje con el valor dado y a que corresponde convertido a la opcion selccionada
+        loop de menu siempre que no de a la opcion 7
+        */
+    }
+}
